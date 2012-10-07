27f603eeda6ebed8b37a72710c1542a479820b13
jrobcary <jrobcary@5e004b58-9d7b-479e-b6d0-c1b89f69eb0b>
2012-10-07 10:24:20 +0000

    Improve installation script to work outside.
    git-svn-id: http://svn.code.sf.net/p/numfig/code-0/trunk@7 5e004b58-9d7b-479e-b6d0-c1b89f69eb0b

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# Locate sphinx
 sphinxinit=`python -c "import sphinx; print sphinx.__file__"`
 if test -z "$sphinxinit"; then
   echo "Sphinx not found.  Quitting."
   exit 1
 fi
 sphinxextdir=`dirname $sphinxinit`/ext
-cp numfig.py $sphinxextdir
+cmd="rm -f $sphinxextdir/numfig.py*"
+echo $cmd
+$cmd
+mydir=`dirname $0`
+mydir=`(cd $mydir; pwd -P)`
+cmd="cp $mydir/numfig.py $sphinxextdir"
+echo $cmd
+$cmd
+# Compile to bytecode
+cmd="python -c \"import py_compile;  py_compile.compile('${sphinxextdir}/numfig.py')\""
+echo "$cmd"
+eval "$cmd"
+# Fix perms
+chmod 664 ${sphinxextdir}/numfig.py*
 
